383816c5bdaf345a72cfa426b4dc44e97bb022c0
Hemant Kumar Singh <hemant@Hemants-MacBook-Air.local>
2021-02-03 15:29:21 +0530

    edit post - traditional

---
+++ src/app/posts/components/edit-post/edit-post.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { PostService } from '../../services/post.service';
+import { PostInterface } from '../../store/types/post.interface';
+
+@Component({
+  selector: 'app-edit-post',
+  templateUrl: './edit-post.component.html',
+  styleUrls: ['./edit-post.component.scss'],
+})
+export class EditPostComponent implements OnInit, OnDestroy {
+  post: PostInterface;
+  sub: Subscription;
+  updateSub: Subscription;
+  constructor(
+    private postService: PostService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.fetcheData();
+  }
+
+  fetcheData() {
+    const postId = this.route.snapshot.paramMap.get('postId');
+    this.sub = this.postService
+      .getPost(postId)
+      .subscribe((post: PostInterface) => {
+        this.post = post;
+      });
+  }
+  updatePost(post) {
+    this.updateSub = this.postService
+      .updatePost(this.post.id, post)
+      .subscribe(() => {
+        this.router.navigateByUrl('/posts');
+      });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+    this.updateSub.unsubscribe();
+  }
+}
